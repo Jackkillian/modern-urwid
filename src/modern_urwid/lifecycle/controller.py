561ee2fd67fc912bceb1568ff85f513755e9bbaa
Jack <jackkillian@tuta.com>
2026-01-09 11:51:12 -0600

    Fix singleton pattern in Controller

--- src/modern_urwid/lifecycle/controller.py
+++ src/modern_urwid/lifecycle/controller.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Union, Any
 
 import urwid
 
@@ -10,21 +10,36 @@ if TYPE_CHECKING:
     from modern_urwid.widgets.builder import WidgetBuilder
 
 
-class Controller(object):
+class SingletonMeta(type):
+    _instances: dict[type, Any] = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        else:
+            cls._instances[cls]._setup(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Controller(metaclass=SingletonMeta):
     """
     Utility class to handle lifecycle hooks and extra UI logic
     """
 
-    _state = {}
-
     def __init__(
-        self,
+            self,
+            manager: Union["LifecycleManager", None] = None,
+            context: Union["CompileContext", None] = None,
+        ):
+            if not hasattr(self, "name"):
+                self.name = None
+            self._setup(manager, context)
+
+    def _setup(self,
         manager: Union["LifecycleManager", None] = None,
         context: Union["CompileContext", None] = None,
     ):
-        self.__dict__ = self._state
-        if not hasattr(self, "name"):
-            self.name = None
         if context is not None:
             self.context = context
             self.local_data = context.get_local(self.name)
